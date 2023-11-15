3b4a9c0064cd71e42880073dbd758354c9b23daa
Adam Age <adamwage@hotmail.com>
2023-11-15 02:39:48 -0600

    updating default options for Tanstack Query

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ import { ThemeProvider } from 'styled-components';
 import router from './Route'
 import { theme } from '../theme';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retryOnMount: false,
+      staleTime: 1000 * 60 * 5,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false, // default: true
+    },
+  },
+});
 
 function App() {
   return (
