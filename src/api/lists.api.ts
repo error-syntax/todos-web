aa15e03a7ac355e269da033770872206727213ba
Adam Age <adamwage@hotmail.com>
2023-11-17 23:52:53 -0600

    delete and update list api calls added

--- src/api/lists.api.ts
+++ src/api/lists.api.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 import { listsSignal } from '../signals/lists.signals';
-import { type Nullable } from './types';
-import { type CreateListInput, type List } from './types/list.types';
+import {
+  type CreateListInput,
+  type CreateListResponse,
+  type UpdateListInput,
+  type UpdateListResponse,
+} from './types/list.types';
 
 export const fetchUserLists = async (userId?: number | null) => {
   if (userId == null) throw new Error('Please provide a user Id');
@@ -19,14 +23,38 @@ export const fetchUserLists = async (userId?: number | null) => {
   return res;
 };
 
-export const createList = async (input: Nullable<CreateListInput>) => {
-  const res = await axios.post<List[]>(
+export const createList = async (input: CreateListInput) => {
+  const res = await axios.post<CreateListResponse>(
     `${import.meta.env.VITE_API_URL}/lists/create`,
     { ...input },
     { withCredentials: true },
   );
 
   listsSignal.value = [...listsSignal.value, res.data[0]];
+
+  return res;
+};
+
+export const deleteList = async (input: number[]) => {
+  const res = await axios.delete<{
+    numOfRowsAffected: number;
+    deletedIds: Array<{ listId: number }>;
+  }>(`${import.meta.env.VITE_API_URL}/lists`, {
+    data: {
+      listIds: input,
+    },
+    withCredentials: true,
+  });
+
+  return res;
+};
+
+export const updateList = async (input: UpdateListInput) => {
+  const res = await axios.put<UpdateListResponse>(
+    `${import.meta.env.VITE_API_URL}/lists`,
+    { ...input },
+    { withCredentials: true },
+  );
 
   return res;
 };
