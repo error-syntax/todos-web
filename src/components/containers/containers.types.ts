8133f90bfe4a1fde25461a7c441bb057e9325074
Adam Age <adamwage@hotmail.com>
2023-11-19 21:09:59 -0600

    shadcn ui comps

--- src/components/containers/containers.types.ts
+++ src/components/containers/containers.types.ts
@@ -1,10 +1,14 @@
 import { type AppendPrefix } from '../../api/types/utility.types';
+import { type useTheme } from '../theme-provider';
+
+type ThemeState = ReturnType<typeof useTheme>['theme'];
 
 type ContainerProps = AppendPrefix<
   '$',
   {
     height?: number;
     padding?: number;
+    theme?: ThemeState;
     width?: number;
   }
 >;
