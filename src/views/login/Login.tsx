334db6596f4bc3dfe294e493056b2293263f3b72
Adam Age <adamwage@hotmail.com>
2023-11-19 21:10:55 -0600

    Login form with form and functionality moved out

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,127 +1,96 @@
-import {
-  type ToPathOption,
-  useNavigate,
-  useRouter,
-} from '@tanstack/react-router';
-import { ErrorMessage, Formik, type FormikConfig } from 'formik';
-import { z } from 'zod';
-import { toFormikValidate } from 'zod-formik-adapter';
+import { Link } from '@tanstack/react-router';
 
-import { logInUser } from '../../api';
-import { type UserLogin } from '../../api/types';
-import { Column, Row, Spacer } from '../../components/containers';
-import { Button, ErrorWrapper, Input, Label } from '../../components/inputs';
-import { userContext } from '../../signals/users.signals';
-import { Wrapper } from './Login.styles';
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
 
-const LOGIN_SCHEMA = z.object({
-  email: z
-    .string()
-    .email('Invalid Email')
-    .min(1, 'Please provide your email to login.'),
-  password: z.string().min(1, 'Please provide your password to login.'),
-});
+import LoginForm from './form';
 
 export default function Login() {
-  const navigate = useNavigate({ from: '/login' });
-  const {
-    state: {
-      location: { search },
-    },
-  } = useRouter();
-  const redirectURL: ToPathOption =
-    (search as Partial<{ redirect: string }>).redirect ?? '/dashboard';
-
-  const handleSubmit: FormikConfig<UserLogin>['onSubmit'] = (
-    values,
-    { setErrors, setSubmitting, validateForm },
-  ) => {
-    void validateForm(values);
-
-    logInUser(values)
-      .then(async (res) => {
-        userContext.value = res;
-        setSubmitting(false);
-      })
-      .then(() => {
-        void navigate({ to: redirectURL });
-      })
-      .catch((err) => {
-        setErrors({ password: err.response.data.errors[0].message });
-        setSubmitting(false);
-      });
-  };
-
   return (
-    <Wrapper>
-      <Column>Image Here</Column>
-      <Column>
-        <h1>Login!</h1>
-        <Spacer $height={40} />
-        <Formik
-          initialValues={{ email: '', password: '' }}
-          onSubmit={handleSubmit}
-          validate={toFormikValidate(LOGIN_SCHEMA)}
-          validateOnChange={false}
-          validateOnBlur={false}
-        >
-          {({
-            values,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-          }) => (
-            <form onSubmit={handleSubmit}>
-              <Row>
-                <Column>
-                  <Label htmlFor="email">Email:</Label>
-                  <Spacer $height={8} />
-                  <Input
-                    autoComplete="off"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    name="email"
-                    type="email"
-                    value={values.email}
-                  />
-                  <ErrorWrapper>
-                    <ErrorMessage name="email" />
-                  </ErrorWrapper>
-                </Column>
-              </Row>
-              <Spacer $height={12} />
-              <Row>
-                <Column>
-                  <Label htmlFor="password">Password:</Label>
-                  <Spacer $height={8} />
-                  <Input
-                    autoComplete="off"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    name="password"
-                    type="password"
-                    value={values.password}
-                  />
-                  <ErrorWrapper>
-                    <ErrorMessage name="password" />
-                  </ErrorWrapper>
-                </Column>
-              </Row>
-              <Spacer $height={12} />
-              <Row>
-                <Button
-                  disabled={isSubmitting}
-                  style={{ flex: 1 }}
-                  type="submit"
-                >
-                  Login
-                </Button>
-              </Row>
-            </form>
+    <>
+      <div className="md:hidden lg:grid-cols-2">
+        <img
+          src="https://place-hold.it/300x500"
+          width={1280}
+          height={843}
+          alt="Authentication"
+          className="block dark:hidden"
+        />
+        <img
+          src="https://place-hold.it/300x500"
+          width={1280}
+          height={843}
+          alt="Authentication"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <Link
+          href="/examples/authentication"
+          className={cn(
+            buttonVariants({ variant: 'ghost' }),
+            'absolute right-4 top-4 md:right-8 md:top-8',
           )}
-        </Formik>
-      </Column>
-    </Wrapper>
+        >
+          Login
+        </Link>
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-6 w-6"
+            >
+              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+            </svg>
+            Todoist
+          </div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;This library has saved me countless hours of work and
+                helped me deliver stunning designs to my clients faster than
+                ever before.&rdquo;
+              </p>
+              <footer className="text-sm">Sofia Davis</footer>
+            </blockquote>
+          </div>
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">Login</h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your credentials to login.
+              </p>
+            </div>
+            <LoginForm />
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{' '}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{' '}
+              and{' '}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
---
+++ src/views/login/form/LoginForm.tsx
@@ -0,0 +1,98 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  type ToPathOption,
+  useNavigate,
+  useRouter,
+} from '@tanstack/react-router';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { logInUser } from '@/api';
+import { Input } from '@/components/inputs';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { userContext } from '@/signals/users.signals';
+
+const LOGIN_SCHEMA = z.object({
+  email: z
+    .string()
+    .email('Invalid Email')
+    .min(1, 'Please provide your email to login.'),
+  password: z.string().min(1, 'Please provide your password to login.'),
+});
+
+export default function LoginForm() {
+  const navigate = useNavigate({ from: '/login' });
+  const {
+    state: {
+      location: { search },
+    },
+  } = useRouter();
+  const redirectURL: ToPathOption =
+    (search as Partial<{ redirect: string }>).redirect ?? '/dashboard';
+
+  const form = useForm<z.infer<typeof LOGIN_SCHEMA>>({
+    resolver: zodResolver(LOGIN_SCHEMA),
+  });
+
+  const onSubmit = (values: z.infer<typeof LOGIN_SCHEMA>) => {
+    logInUser(values)
+      .then(async (res) => {
+        userContext.value = res;
+        return res;
+      })
+      .then((res) => {
+        void navigate({ to: redirectURL });
+        return res;
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={(e) => {
+          void form.handleSubmit(onSubmit)(e);
+        }}
+        className="space-y-8 flex flex-col"
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className="flex flex-col space-y-4">
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="youremail@email.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem className="flex flex-col space-y-4">
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="********" {...field} type="password" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
